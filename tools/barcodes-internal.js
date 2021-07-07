ccdd97101d095180cf3afe29f15b7d30a3b5abd5
TechieHelper <oliverlford@icloud.com>
2021-07-07 15:08:27 +0100

    Very harcoded barcode

--- tools/barcodes-internal.js
+++ tools/barcodes-internal.js
@@ -81,9 +81,21 @@ const QRCODE_LOG_TABLE = {
     "0": 1, "1": 2, "2": 4, "3": 8, "4": 16, "5": 32, "6": 64, "7": 128, "8": 29, "9": 58, "10": 116, "11": 232, "12": 205, "13": 135, "14": 19, "15": 38, "16": 76, "17": 152, "18": 45, "19": 90, "20": 180, "21": 117, "22": 234, "23": 201, "24": 143, "25": 3, "26": 6, "27": 12, "28": 24, "29": 48, "30": 96, "31": 192, "32": 157, "33": 39, "34": 78, "35": 156, "36": 37, "37": 74, "38": 148, "39": 53, "40": 106, "41": 212, "42": 181, "43": 119, "44": 238, "45": 193, "46": 159, "47": 35, "48": 70, "49": 140, "50": 5, "51": 10, "52": 20, "53": 40, "54": 80, "55": 160, "56": 93, "57": 186, "58": 105, "59": 210, "60": 185, "61": 111, "62": 222, "63": 161, "64": 95, "65": 190, "66": 97, "67": 194, "68": 153, "69": 47, "70": 94, "71": 188, "72": 101, "73": 202, "74": 137, "75": 15, "76": 30, "77": 60, "78": 120, "79": 240, "80": 253, "81": 231, "82": 211, "83": 187, "84": 107, "85": 214, "86": 177, "87": 127, "88": 254, "89": 225, "90": 223, "91": 163, "92": 91, "93": 182, "94": 113, "95": 226, "96": 217, "97": 175, "98": 67, "99": 134, "100": 17, "101": 34, "102": 68, "103": 136, "104": 13, "105": 26, "106": 52, "107": 104, "108": 208, "109": 189, "110": 103, "111": 206, "112": 129, "113": 31, "114": 62, "115": 124, "116": 248, "117": 237, "118": 199, "119": 147, "120": 59, "121": 118, "122": 236, "123": 197, "124": 151, "125": 51, "126": 102, "127": 204, "128": 133, "129": 23, "130": 46, "131": 92, "132": 184, "133": 109, "134": 218, "135": 169, "136": 79, "137": 158, "138": 33, "139": 66, "140": 132, "141": 21, "142": 42, "143": 84, "144": 168, "145": 77, "146": 154, "147": 41, "148": 82, "149": 164, "150": 85, "151": 170, "152": 73, "153": 146, "154": 57, "155": 114, "156": 228, "157": 213, "158": 183, "159": 115, "160": 230, "161": 209, "162": 191, "163": 99, "164": 198, "165": 145, "166": 63, "167": 126, "168": 252, "169": 229, "170": 215, "171": 179, "172": 123, "173": 246, "174": 241, "175": 255, "176": 227, "177": 219, "178": 171, "179": 75, "180": 150, "181": 49, "182": 98, "183": 196, "184": 149, "185": 55, "186": 110, "187": 220, "188": 165, "189": 87, "190": 174, "191": 65, "192": 130, "193": 25, "194": 50, "195": 100, "196": 200, "197": 141, "198": 7, "199": 14, "200": 28, "201": 56, "202": 112, "203": 224, "204": 221, "205": 167, "206": 83, "207": 166, "208": 81, "209": 162, "210": 89, "211": 178, "212": 121, "213": 242, "214": 249, "215": 239, "216": 195, "217": 155, "218": 43, "219": 86, "220": 172, "221": 69, "222": 138, "223": 9, "224": 18, "225": 36, "226": 72, "227": 144, "228": 61, "229": 122, "230": 244, "231": 245, "232": 247, "233": 243, "234": 251, "235": 235, "236": 203, "237": 139, "238": 11, "239": 22, "240": 44, "241": 88, "242": 176, "243": 125, "244": 250, "245": 233, "246": 207, "247": 131, "248": 27, "249": 54, "250": 108, "251": 216, "252": 173, "253": 71, "254": 142, "255": 1
 }
 
+const QRCODE_MASKS = {
+    0: (i, j) => (i + j) % 2 == 0, 1: (i, j) => i % 2 == 0, 2: (i, j) => j % 3 == 0, 3: (i, j) => (i + j) % 3 == 0, 4: (i, j) => (i / 2 + j / 3) % 2 == 0, 5: (i, j) => ((i * j) % 2) + ((i * j) % 3) == 0, 6: (i, j) => (((i * j) % 3) + (i * j)) % 2 == 0, 7: (i, j) => (((i * j) % 3) + i + j) % 2 == 0
+}
+
 String.prototype.removeCharIfExists = function (char) {
     if (this.charAt(this.length - 1) == char) {
         return this.substr(0, this.length - 1);
+    } else {
+        return this;
+    }
+}
+
+String.prototype.removeFromStart = function (char) {
+    if (this.charAt(0) == char) {
+        return this.substr(1).removeCharIfExists(char);
     } else {
         return this;
     }
@@ -356,6 +368,18 @@ function dqrcode(code) {
 }
 
 function eqrcode(code) {
+    function arrToBitstring(arr) {
+        return arr.join("");
+    }
+
+    function arr2dtoBitstring(arr, index) {
+        let _bs = "";
+        for (let i = 0; i < arr.length; i++) {
+            _bs += arr[i][index].toString();
+        }
+        return _bs;
+    }
+
     function setupQRCode(_modules) {
         let size = _modules.length - 1;
         for (let i = 0; i < 7; i++) {
@@ -395,6 +419,139 @@ function eqrcode(code) {
         return _modules;
     }
 
+    function determineMaskPenalty(_modules) {
+        let score1 = 0;
+        for (let i = 0; i < _modules.length; i++) {
+            let prev = -1;
+            let run = 0;
+            for (let j = 0; j < _modules[i].length; j++) {
+                let current = _modules[j][i];
+                if (current == prev) {
+                    run++;
+                } else {
+                    if (run >= 5) {
+                        score1 += 3 + (run - 5);
+                    }
+                    run = 1;
+                    prev = current;
+                }
+            }
+            if (run >= 5) {
+                score1 += 3 + (run - 5);
+            }
+        }
+        for (let i = 0; i < _modules.length; i++) {
+            let prev = -1;
+            let run = 0;
+            for (let j = 0; j < _modules[i].length; j++) {
+                let current = _modules[i][j];
+                if (current == prev) {
+                    run++;
+                } else {
+                    if (run >= 5) {
+                        score1 += 3 + (run - 5);
+                    }
+                    run = 1;
+                    prev = current;
+                }
+            }
+            if (run >= 5) {
+                score1 += 3 + (run - 5);
+            }
+        }
+
+        let score2 = 0;
+
+        for (let i = 0; i < _modules.length; i++) {
+            for (let j = 0; j < _modules[i].length; j++) {
+                let current = _modules[j][i];
+                try {
+                    if (_modules[j][i+1] == current && _modules[j+1][i] == current && _modules[j+1][i+1] == current) {
+                        score2 += 3;
+                    }
+                } catch (error) {
+                    
+                }
+                
+            }
+        }
+
+        let score3 = 0;
+
+        for (let i = 0; i < _modules.length; i++) {
+            let bs = arrToBitstring(_modules[i]);
+            score3 += (bs.match(/10111010000/g) || []).length * 40;
+            score3 += (bs.match(/00001011101/g) || []).length * 40;
+
+            bs = arr2dtoBitstring(_modules, i);
+            score3 += (bs.match(/10111010000/g) || []).length * 40;
+            score3 += (bs.match(/00001011101/g) || []).length * 40;
+        }
+
+        let score4 = 0;
+        let totalSize = _modules.length * _modules[0].length;
+        let darkmodules = 0;
+        for (let i = 0; i < _modules.length; i++) {
+            for (let j = 0; j < _modules.length; j++) {
+                darkmodules += parseInt(_modules[i][j]);
+            }
+        }
+        let darkmodulesPercentage = (darkmodules / totalSize) * 100;
+        let upper = Math.abs((Math.ceil(darkmodulesPercentage / 5) * 5) - 50) / 5;
+        let lower = Math.abs((Math.floor(darkmodulesPercentage / 5) * 5) - 50) / 5;
+        
+        score4 = (upper < lower ? upper * 10 : lower * 10);
+
+        return score1 + score2 + score3 + score4;
+    }
+
+    function applyMask(_modules, mask) {
+        let size = _modules.length - 1;
+        let mods = _modules;
+        console.log(mods[20][19]);
+        for (let i = 0; i < mods.length; i++) {
+            for (let j = 0; j < mods[0].length; j++) {
+                if (!((i < 8 && j < 8) || (i > size - 8 && j < 8) || (i < 8 && j > size - 8) || (i == 8 && j == size - 7) || (i == 6) || (j == 6))) {
+                    console.log(i, j, mods[i][j]);
+                    if (mask(i, j)) {
+                        console.log(i, j, mods[i][j]);
+                        mods[i][j] = 1 - mods[i][j];
+                        console.log(i, j, mods[i][j]);
+                    }
+                }
+            }
+        }
+        return _modules;
+    }
+
+    function fillFormatString(_modules, format) {
+        let size = _modules.length - 1;
+
+        let pos = 0;
+        for (let i = 0; i < 7; i++) {
+            _modules[8][size - i] = parseInt(format[pos++]);
+        }
+        for (let i = 0; i < 8; i++) {
+            _modules[size - (7 - i)][8] = parseInt(format[pos++]);
+        }
+
+        pos = 0;
+        for (let i = 0; i < 6; i++) {
+            _modules[i][8] = parseInt(format[pos++]);
+        }
+        _modules[7][8] = parseInt(format[pos++]);
+        _modules[8][8] = parseInt(format[pos++]);
+        _modules[8][7] = parseInt(format[pos++]);
+
+        for (let i = 0; i < 6; i++) {
+            _modules[8][5 - i] = parseInt(format[pos++]);
+        }
+
+        return _modules;
+    }
+
+    code = "HELLO WORLD";
+
     // CURRENTLY ONLY ALPHANUMERIC MODE
     let ecl = "Q";  // Error Correction Level
     let V = 1;  // version
@@ -421,21 +578,19 @@ function eqrcode(code) {
     bitstring += "00";  // NOT DYNAMIC
     bitstring += "111011000001000111101100";  // NOT DYNAMIC
 
-    bitstring = "00100000010110110000101101111000110100010111001011011100010011010100001101000000111011000001000111101100000100011110110000010001";
-
     let g1b1 = bitstring.match(/.{1,8}/g);
     for (let i = 0; i < g1b1.length; i++) {
         g1b1[i] = parseInt(g1b1[i], 2);
     }
 
-    for (let i = 0; i < 16; i++) {
-        let generator = [0, 251, 67, 46, 61, 118, 70, 64, 94, 32, 45];
+    for (let i = 0; i < 13; i++) {
+        let generator = [0, 74, 152, 176, 100, 86, 100, 106, 104, 130, 218, 206, 140, 78];
         let alpha = QRCODE_ANTILOG_TABLE[g1b1[0].toString()];
         for (let j = 0; j < generator.length; j++) {
             generator[j] = QRCODE_LOG_TABLE[(generator[j] + alpha) % 255];
         }
 
-        if (g1b1.length <= 10) g1b1.push(0);
+        if (g1b1.length <= 13) g1b1.push(0);
         
         for (let j = 0; j < g1b1.length; j++) {
             g1b1[j] = generator[j] ^ g1b1[j];
@@ -456,74 +611,111 @@ function eqrcode(code) {
         }
     }
 
-    // bitstring: 0010000001011011000010110111100011010001011100101101110001001101010000110100000011101100000100011110110000010001111011000001000111000100001000110010011101110111111010111101011111100111111000100101110100010111
-
+    //bitstring = "0010000001011011000010110111100011010001011100101101110001001101010000110100000011101100000100011110110000010001111011000001000111000100001000110010011101110111111010111101011111100111111000100101110100010111"
     modules = setupQRCode(modules);
 
     let pos = 0;
 
     let s = 20;  // size
 
+    console.log(bitstring);  // 0010000001011011000010110111100011010001011100101101110001001101010000110100000011101100000100011110110010101000010010000001011001010010110110010011011010011100000000000010111000001111101101000111101000010000
+
     for (let i = 0; i < 12; i++) {
-        modules[s][s-i] = bitstring[pos++];
-        modules[s-1][s-i] = bitstring[pos++];
+        modules[s][s-i] = parseInt(bitstring[pos++]);
+        modules[s-1][s-i] = parseInt(bitstring[pos++]);
     }
 
     for (let i = 0; i < 12; i++) {
-        modules[s-2][s-(11-i)] = bitstring[pos++];
-        modules[s-3][s-(11-i)] = bitstring[pos++];
+        modules[s-2][s-(11-i)] = parseInt(bitstring[pos++]);
+        modules[s-3][s-(11-i)] = parseInt(bitstring[pos++]);
     }
 
     for (let i = 0; i < 12; i++) {
-        modules[s-4][s-i] = bitstring[pos++];
-        modules[s-5][s-i] = bitstring[pos++];
+        modules[s-4][s-i] = parseInt(bitstring[pos++]);
+        modules[s-5][s-i] = parseInt(bitstring[pos++]);
     }
 
     for (let i = 0; i < 12; i++) {
-        modules[s-6][s-(11-i)] = bitstring[pos++];
-        modules[s-7][s-(11-i)] = bitstring[pos++];
+        modules[s-6][s-(11-i)] = parseInt(bitstring[pos++]);
+        modules[s-7][s-(11-i)] = parseInt(bitstring[pos++]);
     }
     
     for (let i = 0; i < 14; i++) {
-        modules[s-8][s-i] = bitstring[pos++];
-        modules[s-9][s-i] = bitstring[pos++];
+        modules[s-8][s-i] = parseInt(bitstring[pos++]);
+        modules[s-9][s-i] = parseInt(bitstring[pos++]);
     }
     for (let i = 5; i >= 0; i--) {
-        modules[s-8][i] = bitstring[pos++];
-        modules[s-9][i] = bitstring[pos++];
+        modules[s-8][i] = parseInt(bitstring[pos++]);
+        modules[s-9][i] = parseInt(bitstring[pos++]);
     }
 
     for (let i = 0; i < 6; i++) {
-        modules[s-10][i] = bitstring[pos++];
-        modules[s-11][i] = bitstring[pos++];
+        modules[s-10][i] = parseInt(bitstring[pos++]);
+        modules[s-11][i] = parseInt(bitstring[pos++]);
     }
     for (let i = 0; i < 14; i++) {
-        modules[s-10][s-(13-i)] = bitstring[pos++];
-        modules[s-11][s-(13-i)] = bitstring[pos++];
+        modules[s-10][s-(13-i)] = parseInt(bitstring[pos++]);
+        modules[s-11][s-(13-i)] = parseInt(bitstring[pos++]);
     }
 
     for (let i = 0; i < 4; i++) {
-        modules[8][s-i-8] = bitstring[pos++];
-        modules[7][s-i-8] = bitstring[pos++];
+        modules[8][s-i-8] = parseInt(bitstring[pos++]);
+        modules[7][s-i-8] = parseInt(bitstring[pos++]);
     }
 
     for (let i = 0; i < 4; i++) {
-        modules[5][i+8] = bitstring[pos++];
-        modules[4][i+8] = bitstring[pos++];
+        modules[5][i+8] = parseInt(bitstring[pos++]);
+        modules[4][i+8] = parseInt(bitstring[pos++]);
     }
 
     for (let i = 0; i < 4; i++) {
-        modules[3][s-i-8] = bitstring[pos++];
-        modules[2][s-i-8] = bitstring[pos++];
+        modules[3][s-i-8] = parseInt(bitstring[pos++]);
+        modules[2][s-i-8] = parseInt(bitstring[pos++]);
     }
 
     for (let i = 0; i < 4; i++) {
-        modules[1][i+8] = bitstring[pos++];
-        modules[0][i+8] = bitstring[pos++];
+        modules[1][i+8] = parseInt(bitstring[pos++]);
+        modules[0][i+8] = parseInt(bitstring[pos++]);
     }
 
+    console.log(modules);
+
+    let scores = [];
+    //scores.push(determineMaskPenalty(applyMask(modules, QRCODE_MASKS[0])));
+    //scores.push(determineMaskPenalty(applyMask(modules, QRCODE_MASKS[1])));
+    //scores.push(determineMaskPenalty(applyMask(modules, QRCODE_MASKS[2])));
+    //scores.push(determineMaskPenalty(applyMask(modules, QRCODE_MASKS[3])));
+    //scores.push(determineMaskPenalty(applyMask(modules, QRCODE_MASKS[4])));
+    //scores.push(determineMaskPenalty(applyMask(modules, QRCODE_MASKS[5])));
+    //scores.push(determineMaskPenalty(applyMask(modules, QRCODE_MASKS[6])));
+    //scores.push(determineMaskPenalty(applyMask(modules, QRCODE_MASKS[7])));
+
+    //let bestMask = scores.indexOf(Math.min(...scores));
+
+    let bestMask = 6;
+
+    modules = applyMask(modules, QRCODE_MASKS[bestMask]);
+
+    let formatString = "00" + bestMask.toString(2).padStart(3, "0") + "0000000000";
+
+    //formatString = formatString.removeFromStart("0");
+
+    // let generator = "10100110111";
+    // generator.padEnd(formatString.length, "0");
+    // let newString = "";
+    // for (let i = 0; i < generator.length; i++) {
+    //     newString += (parseInt(generator[i]) ^ parseInt(formatString[i])).toString();
+    // }
+
+    let finalFormatString = "";
+    let mask = "101010000010010";
+    for (let i = 0; i < formatString.length; i++) {
+        finalFormatString += (parseInt(formatString[i]) ^ parseInt(mask[i])).toString();
+    }
+
+    finalFormatString = "010111011011010"
+
+    modules = fillFormatString(modules, finalFormatString);
 
     return generatePNGFromBitArray(modules);
 }
-
-//eqrcode("HELLO WORLD")
