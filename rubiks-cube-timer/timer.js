57fb54c37a4fe612db47416c71b4a55079afc135
TechieHelper <oliverlford@icloud.com>
2021-10-20 07:43:48 +0100

    Basic Scrambler

--- rubiks-cube-timer/timer.js
+++ rubiks-cube-timer/timer.js
@@ -10,12 +10,18 @@ function setDarkMode() {
         --bg-color: #171717;
         --bg-box-color: #272727;
         --border-color: rgba(255,255,255,.125);
-        --modal-bg-color: #fff;
+        --modal-bg-color: #171717;
     }`);
 }
 
 function unsetDarkMode() {
-    sheet.replaceSync("body {--text-color: #212529;}");
+    sheet.replaceSync(`body {
+        --text-color: #212529;
+        --bg-color: #f5f5f5;
+        --bg-box-color: #f0f0f0;
+        --border-color: rgba(0,0,0,.125);
+        --modal-bg-color: #fff;
+    }`);
 }
 
 function updateSettings() {
@@ -26,8 +32,21 @@ function updateSettings() {
     }
 }
 
+function timerFinished(finalTime) {
+    let table = document.getElementById("times-table");
+    let row = table.insertRow(-1);
+    let timeCell = row.insertCell(0);
+    timeCell.innerHTML = formatTime(finalTime);
+
+    document.getElementById("scrambleText").innerHTML = generate333();
+}
+
 function formatTime(time) {
-    return Math.floor((time / 1000) / 60) + ":" + ((time / 1000) % 60).toFixed(2).padStart(5, '0');
+    if (Math.floor((time / 1000) / 60) > 0) {
+        return Math.floor((time / 1000) / 60) + ":" + ((time / 1000) % 60).toFixed(2);
+    } else {
+        return ((time / 1000) % 60).toFixed(2);
+    }
 }
 
 var timerFunction = function() {
@@ -36,16 +55,48 @@ var timerFunction = function() {
 };
 
 var intervalID = 0;
+var spaceStartTime = Date.now();
+var currentlyDown = false;
+var endingSpace = false;
 
-$(document).on("keypress", function(e) {
+document.getElementById("scrambleText").innerHTML = generate333(); // Setup
+
+$(document).on("keydown", function(e) {
     if (e.which == 32) {
         if (!running) {
-            startTime = Date.now();
-            intervalID = setInterval(timerFunction, 16);
-            running = true;
-        } else {
+            if (!currentlyDown) {
+                spaceStartTime = Date.now();
+                currentlyDown = true;
+            }
+        } else { // Stop as soon as possible, on keydown
+            let finalTime = Date.now() - startTime;
             clearInterval(intervalID);
+            document.getElementById("timer").innerHTML = formatTime(finalTime); // In case .01 difference occurs due to timer stop time
+
+            timerFinished(finalTime);
             running = false;
+            endingSpace = true;
+        }
+    }
+});
+
+$(document).on("keyup", function(e) {
+    if (e.which == 32) {
+        currentlyDown = false;
+        if (endingSpace) {
+            endingSpace = false;
+            return;
+        }
+        if (!running) {
+            if (Date.now() - spaceStartTime > 500) {
+                ready = false;
+                startTime = Date.now();
+                intervalID = setInterval(timerFunction, 16);
+                running = true;
+            } else {
+                spaceStartTime = Infinity;
+                console.log("Released too early.");
+            }
         }
     }
 });
