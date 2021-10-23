17e66addb465aec72e333b91ec7eacfeb1da5750
TechieHelper <oliverlford@icloud.com>
2021-10-23 15:55:23 +0100

    ActuallyApply is the worst ever function...

---
+++ speedcubing/cube.js
@@ -0,0 +1,71 @@
+const notationToRot = {"'": 3, undefined: 1, "2": 2}
+
+class Cube {
+    constructor(cubeInstructions) { // wyogrb
+        this.cubeData = [];
+        for (let i = 0; i < 6; i++) {
+            this.cubeData.push(cubeInstructions.substr(i * 9, 9).split(""));
+        }
+    }
+
+    actuallyApply(a, b, c, d, aa, ab, ac, ba, bb, bc, ca, cb, cc, da, db, dc, rotface) {
+        let temp = [this.cubeData[a][aa], this.cubeData[a][ab], this.cubeData[a][ac]];
+        [this.cubeData[a][aa], this.cubeData[a][ab], this.cubeData[a][ac]] = [this.cubeData[d][da], this.cubeData[d][db], this.cubeData[d][dc]];
+        [this.cubeData[d][da], this.cubeData[d][db], this.cubeData[d][dc]] = [this.cubeData[c][ca], this.cubeData[c][cb], this.cubeData[c][cc]];
+        [this.cubeData[c][ca], this.cubeData[c][cb], this.cubeData[c][cc]] = [this.cubeData[b][ba], this.cubeData[b][bb], this.cubeData[b][bc]];
+        [this.cubeData[b][ba], this.cubeData[b][bb], this.cubeData[b][bc]] = [temp[0], temp[1], temp[2]];
+
+        if (rotface != -1) {
+            temp = this.cubeData[rotface][3];
+            this.cubeData[rotface][3] = this.cubeData[rotface][7];
+            this.cubeData[rotface][7] = this.cubeData[rotface][5];
+            this.cubeData[rotface][5] = this.cubeData[rotface][1];
+            this.cubeData[rotface][1] = temp;
+
+            temp = this.cubeData[rotface][0];
+            this.cubeData[rotface][0] = this.cubeData[rotface][6];
+            this.cubeData[rotface][6] = this.cubeData[rotface][8];
+            this.cubeData[rotface][8] = this.cubeData[rotface][2];
+            this.cubeData[rotface][2] = temp;
+        }
+    }
+
+    applyMove(move) {
+        switch (move[0]) {
+        case "R":
+            for (let i = 0; i < notationToRot[move[1]]; i++)
+                this.actuallyApply(0, 5, 1, 3,  2, 5, 8,  6, 3, 0,  2, 5, 8,  2, 5, 8,  4)
+            break;
+        case "L":
+            for (let i = 0; i < notationToRot[move[1]]; i++)
+                this.actuallyApply(0, 3, 1, 5,  0, 3, 6,  0, 3, 6,  0, 3, 6,  8, 5, 2,  2)
+            break;
+        case "U":
+            for (let i = 0; i < notationToRot[move[1]]; i++)
+                this.actuallyApply(2, 5, 4, 3,  0, 1, 2,  0, 1, 2,  0, 1, 2,  0, 1, 2,  0)
+            break;
+        case "D":
+            for (let i = 0; i < notationToRot[move[1]]; i++)
+                this.actuallyApply(2, 3, 4, 5,  6, 7, 8,  6, 7, 8,  6, 7, 8,  6, 7, 8,  1)
+            break;
+        case "F":
+            for (let i = 0; i < notationToRot[move[1]]; i++)
+                this.actuallyApply(0, 4, 1, 2,  6, 7, 8,  0, 3, 6,  2, 1, 0,  8, 5, 2,  3)
+            break;
+        case "B":
+            for (let i = 0; i < notationToRot[move[1]]; i++)
+                this.actuallyApply(0, 2, 1, 4,  0, 1, 2,  6, 3, 0,  8, 7, 6,  2, 5, 8,  5)
+            break;
+        }
+    }
+
+    applyMoves(moves) {
+        moves.forEach((item, index) => {
+            if (item !== "") this.applyMove(item);
+        });
+    }
+
+    static Empty() {
+        return new Cube("wwwwwwwwwyyyyyyyyyooooooooogggggggggrrrrrrrrrbbbbbbbbb");
+    }
+}
--- speedcubing/timer.js
+++ speedcubing/timer.js
@@ -35,8 +35,67 @@ function unsetDarkMode() {
     }`);
 }
 
+function drawScramble(scrambleData) {
+    const letterToColour = {"w": "white", "y": "yellow", "b": "blue", "g": "green", "r": "red", "o": "orange"}
+    let canvas = document.getElementById("drawn-scramble-canvas");
+    let ctx = canvas.getContext("2d");
+    ctx.clearRect(-0.5, -0.5, canvas.width, canvas.height);
+
+    let scaleFactor = 15;
+    
+    for (let i = 0; i < 3 * scaleFactor; i += scaleFactor) { // white face
+        for (let j = 0; j < 3 * scaleFactor; j += scaleFactor) {
+            ctx.fillStyle = letterToColour[scrambleData[0][(i / scaleFactor) + (j / scaleFactor) * 3]]
+            ctx.beginPath();
+            ctx.moveTo(i + scaleFactor * 3 + 2, j);
+            ctx.lineTo(i + scaleFactor * 3 + scaleFactor + 2, j);
+            ctx.lineTo(i + scaleFactor * 3 + scaleFactor + 2, j + scaleFactor);
+            ctx.lineTo(i + scaleFactor * 3 + 2, j + scaleFactor);
+            ctx.lineTo(i + scaleFactor * 3 + 2, j);
+            ctx.fill();
+            ctx.stroke();
+        }
+    }
+
+    for (let i = 0; i < 3 * scaleFactor; i += scaleFactor) { // yellow face
+        for (let j = 0; j < 3 * scaleFactor; j += scaleFactor) {
+            ctx.fillStyle = letterToColour[scrambleData[1][(i / scaleFactor) + (j / scaleFactor) * 3]]
+            ctx.beginPath();
+            ctx.moveTo(i + scaleFactor * 3 + 2, j + scaleFactor * 6 + 4);
+            ctx.lineTo(i + scaleFactor * 3 + scaleFactor + 2, j + scaleFactor * 6 + 4);
+            ctx.lineTo(i + scaleFactor * 3 + scaleFactor + 2, j + scaleFactor + scaleFactor * 6 + 4);
+            ctx.lineTo(i + scaleFactor * 3 + 2, j + scaleFactor + scaleFactor * 6 + 4);
+            ctx.lineTo(i + scaleFactor * 3 + 2, j + scaleFactor * 6 + 4);
+            ctx.fill();
+            ctx.stroke();
+        }
+    }
+
+    for (let n = 0; n < 4; n++) { // other sides
+        for (let i = 0; i < 3 * scaleFactor; i += scaleFactor) {
+            for (let j = 0; j < 3 * scaleFactor; j += scaleFactor) {
+                ctx.fillStyle = letterToColour[scrambleData[n + 2][(i / scaleFactor) + (j / scaleFactor) * 3]]
+                ctx.beginPath();
+                ctx.moveTo(i + n * scaleFactor * 3 + 2 * n, j + scaleFactor * 3 + 2);
+                ctx.lineTo(i + scaleFactor + n * scaleFactor * 3 + 2 * n, j + scaleFactor * 3 + 2);
+                ctx.lineTo(i + scaleFactor + n * scaleFactor * 3 + 2 * n, j + scaleFactor + scaleFactor * 3 + 2);
+                ctx.lineTo(i + n * scaleFactor * 3 + 2 * n, j + scaleFactor + scaleFactor * 3 + 2);
+                ctx.lineTo(i + n * scaleFactor * 3 + 2 * n, j + scaleFactor * 3 + 2);
+                ctx.fill();
+                ctx.stroke();
+            }
+        }
+    }
+}
+
 function generateCorrectScramble() {
-    document.getElementById("scrambleText").innerHTML = scrambleLookupTable[document.getElementById("scrambler-main").value][document.getElementById("scrambler-sub").value]();
+    let scramble = scrambleLookupTable[document.getElementById("scrambler-main").value][document.getElementById("scrambler-sub").value]();
+    if (document.getElementById("drawn-scramble-check").checked) {
+        currentCubeScramble = Cube.Empty();
+        currentCubeScramble.applyMoves(scramble.split(" "));
+        drawScramble(currentCubeScramble.cubeData);
+    }
+    document.getElementById("scrambleText").innerHTML = scramble;
 }
 
 function updateDarkMode() {
@@ -48,8 +107,19 @@ function updateDarkMode() {
 }
 
 function updateDrawnScramble() {
-    if (document.getElementById("tool-box-check").checked) {
-
+    if (document.getElementById("drawn-scramble-check").checked) {
+        localStorage.setItem("drawnScramble", "1");
+        $("#drawn-scramble").show();
+        currentCubeScramble = Cube.Empty();
+        currentCubeScramble.applyMoves(document.getElementById("scrambleText").innerHTML.split(" "));
+        drawScramble(currentCubeScramble.cubeData);
+        
+    } else {
+        localStorage.setItem("drawnScramble", "0");
+        let canvas = document.getElementById("drawn-scramble-canvas");
+        let ctx = canvas.getContext("2d");
+        ctx.clearRect(-0.5, -0.5, canvas.width, canvas.height);
+        $("#drawn-scramble").hide();
     }
 }
 
@@ -165,18 +235,21 @@ var greenTimerFunction = function() {
 var store;
 var dbPromise;
 var isMobile = false;
+var currentCubeScramble = Cube.Empty();
 
 async function setup() {
     let a = navigator.userAgent||navigator.vendor||window.opera;
     if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) isMobile = true;
 
-    console.log(isMobile);
-
-    document.getElementById("scrambleText").innerHTML = generate333();
-
     if (localStorage.getItem("colorPref") == "dark") {
         setDarkMode();
         document.getElementById("dark-mode-check").checked = true;
+    }
+
+    if (localStorage.getItem("drawnScramble") == "1") {
+        document.getElementById("drawn-scramble-check").checked = true;
+    } else {
+        $("#drawn-scramble").hide();
     }
 
     dbPromise = await idb.openDB("keyval", 1, {
@@ -184,6 +257,12 @@ async function setup() {
             db.createObjectStore('keyval');
         },
     });
+
+    let canvas = document.getElementById("drawn-scramble-canvas");
+    canvas.width = 190;
+    canvas.height = 140;
+    let ctx = canvas.getContext("2d");
+    ctx.translate(0.5, 0.5);
 
     let storedTimes = await get("times");
     let prevTimes = [];
@@ -195,6 +274,8 @@ async function setup() {
             addTime(item["time"], prevTimes);
         });
     }
+
+    generateCorrectScramble();
 }
 
 async function get(key) {
@@ -265,7 +346,7 @@ function keyupFunction() {
             document.getElementById("timer").style.color = "";
         } else {
             spaceStartTime = Infinity;
-            console.log("Released too early.");
+            console.info("Released too early.");
             clearTimeout(greenColourID);
             document.getElementById("timer").style.color = "";
         }
